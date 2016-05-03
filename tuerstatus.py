f3df9503eebd4b949b439d191397747ecf61b10e
Johannes Thorn <johannes.thorn@g4t3.de>
2016-05-03 22:00:24 +0200

    Add mpd pausing if door is locked

--- tuerstatus.py
+++ tuerstatus.py
@@ -1,17 +1,36 @@
 #!/usr/bin/env python
 
 import json
+import os
 import requests
+
+from mpd import MPDClient
 
 LOCK = '334EC1'
 URL = 'http://localhost/status/door'
+
+MPD_HOST = os.environ['MPD_HOST']
+MPD_PORT = os.environ['MPD_PORT']
+MPD_PASS = os.environ['MPD_PASS']
 
 open_last = False
 
 lights = [8, 7, 6, 5, 4, 3, 1]
 
+def handleMPD(is_open):
+	if not is_open:
+		print ("Pausing MPD")
+		client = MPDClient()
+		client.connect(MPD_HOST, MPD_PORT)
+		client.password(MPD_PASS)
+
+		client.pause(1)
+
+		client.close()
+		client.disconnect()
 
 def handleStateChange(is_open):
+	handleMPD(is_open)
 	payload = {'door_open': 1 if is_open else 0}
 	print("Payload ", payload)
 	for x in range(1,5):
