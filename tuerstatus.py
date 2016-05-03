44fd4c8640fe10f6669ebe3f6d3919cc396a53f1
Johannes Thorn <johannes.thorn@g4t3.de>
2016-05-03 21:57:59 +0200

    Extract state changed handler

--- tuerstatus.py
+++ tuerstatus.py
@@ -8,13 +8,29 @@ URL = 'http://localhost/status/door'
 
 open_last = False
 
-lights = [5, 4, 3, 1]
+lights = [8, 7, 6, 5, 4, 3, 1]
+
+
+def handleStateChange(is_open):
+	payload = {'door_open': 1 if is_open else 0}
+	print("Payload ", payload)
+	for x in range(1,5):
+		r = requests.post(URL, data=payload)
+		print(r)
+
+		if r.status_code == requests.codes.ok:
+			break
+
+	if not is_open:
+		for x in lights:
+			url = "http://troll.nobreakspace.org/control?cmd=set_state_actuator&number={0}&function=2".format(x)
+			requests.get(url)
 
 while True:
 	r = requests.get('https://padlock.nobreakspace.org/api/locks/stream', cert=('tuerstatus.crt', 'tuerstatus.key'), stream=True, verify=False)
 
 	for line in r.iter_lines(chunk_size=16):
-		print("line", line)
+		print("Result line ", line)
 		if len(line) > 3:
 			data = line[6:]
 			data = json.loads(data.decode("UTF-8"))
@@ -22,23 +38,10 @@ while True:
 			for d in data:
 				locks[d['id']] = d
 
-			print(locks)
+			# print("Locks data", locks)
 
 			is_open = not locks[LOCK]['locked']
 			if open_last != is_open:
 				open_last = is_open
 
-				payload = {'door_open': 1 if is_open else 0}
-				print("payload", payload)
-				for x in range(1,5):
-					r = requests.post(URL, data=payload)
-					print(r)
-
-					if r.status_code == requests.codes.ok:
-						break
-
-				if not is_open:
-					for x in lights:
-						url = "http://troll.nobreakspace.org/control?cmd=set_state_actuator&number={0}&function=2".format(x)
-						requests.get(url)
-
+				handleStateChange(is_open)
